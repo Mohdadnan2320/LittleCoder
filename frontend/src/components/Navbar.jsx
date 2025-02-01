13473596d57a84385d8ffb54e4dea68572651efa
Mohd Adnan <mohdadnan2320@gmail.com>
2025-02-01 23:09:31 +0530

    menu bar color updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
             <svg
               onClick={() => setShowSidebar(!showSidebar)}
               className="flex items-center cursor-pointer lg:hidden "
-              fill="#2563EB"
+              fill="#0000"
               viewBox="0 0 100 80"
               width="30"
               height="30"
